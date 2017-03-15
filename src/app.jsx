4671a2c48fc825e65a38b88e60a48a0860aa22cd
Akila Chathuranga <akila.13@cse.mrt.ac.lk>
2017-03-15 14:49:20 +0530

    boostrap added

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../styles/index.scss';
-
+import '../styles/bootstrap-3.3.7-dist/css/bootstrap.min.css';
 export default class App extends React.Component {
   render() {
     return (
@@ -8,6 +8,7 @@ export default class App extends React.Component {
         <h1>It Works!</h1>
         <p>This React project just works including test_nimash123<span className="redBg">module</span> local styles.</p>
         <p>Enjoy!</p>
+        <button className="btn btn-default">ok</button>
       </div>
     )
   }
