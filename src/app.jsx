5ab90859f9241c83ee10749b1bb8348d35e4b0d5
NimashDilanka <nimashdilanka.13@cse.mrt.ac.lk>
2017-03-15 14:35:10 +0530

    Nimash_testcommit1

--- src/app.jsx
+++ src/app.jsx
@@ -6,9 +6,11 @@ export default class App extends React.Component {
     return (
       <div>
         <h1>It Works!</h1>
-        <p>This React project just works including <span className="redBg">module</span> local styles.</p>
+        <p>This React project just works including test_nimash123<span className="redBg">module</span> local styles.</p>
         <p>Enjoy!</p>
       </div>
     )
   }
 }
+
+
