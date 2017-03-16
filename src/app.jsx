c5d27da187553971f242b80245364b6eb0ef682c
Akila Chathuranga <akila.13@cse.mrt.ac.lk>
2017-03-16 10:18:50 +0530

    jquery added

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import '../styles/index.scss';
 import '../styles/bootstrap-3.3.7-dist/css/bootstrap.min.css';
+import $ from '../jquery/jquery-3.1.1.min.js';
 export default class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="container-fluid">
+      	<div className="page-header">
+          <h1>Water Management System <small>Rainfall and waterfall prediction</small></h1>
+          <br/>
+          <ul className="nav nav-pills">
+            <li role="presentation" className="active"><a href="#">Home</a></li>
+            <li role="presentation"><a href="#">Profile</a></li>
+            <li role="presentation"><a href="#">Messages</a></li>
+          </ul>
+        </div>
+
         <h1>It Works!</h1>
         <p>This React project just works including test_nimash123<span className="redBg">module</span> local styles.</p>
         <p>Enjoy!</p>
