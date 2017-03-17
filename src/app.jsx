f8f9588764ba8ebc0464a2cee4baacab45356bbb
Akila Chathuranga <akila.13@cse.mrt.ac.lk>
2017-03-17 11:22:40 +0530

    add aprototype of project

--- src/app.jsx
+++ src/app.jsx
@@ -1,25 +1,116 @@
 import React from 'react';
+import Header from './Components/Header.jsx'
 import '../styles/index.scss';
 import '../styles/bootstrap-3.3.7-dist/css/bootstrap.min.css';
 import $ from '../jquery/jquery-3.1.1.min.js';
 export default class App extends React.Component {
+
+	constructor(){
+		super();
+		this.state={navi:1};
+	}
+	handleNavigationCallback(index){
+		this.setState({navi:index})
+	}
+	renderContent(){
+		if(this.state.navi==1){
+			return <div className="container-fluid"> home page detail ....</div>;
+		}
+		if(this.state.navi==2){
+		return (
+			<div className="app-container container-fluid" >
+				<div className="row container-fluid">
+					<div className="col col-sm-6 locationDiv" id="map">
+						<img src="http://srilankamap.facts.co/srilankamapof/SriLankaBlankMap.png" />
+					</div>
+					<div className="col col-sm-6 descriptionDiv" id="locationDetails">
+						<div className="row container-fluid">
+							<div className="col col-sm-5">
+								<h4>
+								Location
+								</h4>
+							</div>
+							<div className="col col-sm-2">
+							<h4>
+							:
+							</h4>
+							</div>
+							<div className="col col-sm-5">
+							<h4>
+							Randenigala
+							</h4>
+							</div>
+						</div>
+						<div className="row container-fluid">
+							<div className="col col-sm-5">
+								<h4>
+								height from sea level
+								</h4>
+							</div>
+							<div className="col col-sm-2">
+							<h4>
+							:
+							</h4>
+							</div>
+							<div className="col col-sm-5">
+							<h4>
+							1345m
+							</h4>
+							</div>
+						</div>
+						<div className="row container-fluid">
+							<div className="col col-sm-5">
+								<h4>
+								water level
+								</h4>
+							</div>
+							<div className="col col-sm-2">
+							<h4>
+							:
+							</h4>
+							</div>
+							<div className="col col-sm-5">
+							<h4>
+							55m
+							</h4>
+							</div>
+						</div>
+						<div className="row container-fluid">
+							<div className="col col-sm-5">
+								<h4>
+								Rainfall
+								</h4>
+							</div>
+							<div className="col col-sm-2">
+							<h4>
+							:
+							</h4>
+							</div>
+							<div className="col col-sm-5">
+							<h4>
+							13mm
+							</h4>
+							</div>
+						</div>
+
+					</div>
+				</div>
+		</div>
+		);
+		}
+		if(this.state.navi==3){
+			return <div className="container-fluid"> no data to show ....</div>;
+		}
+		if(this.state.navi==4){
+			return <div className="container-fluid"> tel: 087-8838455<br/>321,<br/>colombo 10</div>;
+		}
+	}
   render() {
+  	let self= this;
     return (
       <div className="container-fluid">
-      	<div className="page-header">
-          <h1>Water Management System <small>Rainfall and waterfall prediction</small></h1>
-          <br/>
-          <ul className="nav nav-pills">
-            <li role="presentation" className="active"><a href="#">Home</a></li>
-            <li role="presentation"><a href="#">Profile</a></li>
-            <li role="presentation"><a href="#">Messages</a></li>
-          </ul>
-        </div>
-
-        <h1>It Works!</h1>
-        <p>This React project just works including test_nimash123<span className="redBg">module</span> local styles.</p>
-        <p>Enjoy!</p>
-        <button className="btn btn-default">ok</button>
+      	<Header callback={self.handleNavigationCallback.bind(self)}/>
+				{this.renderContent()}
       </div>
     )
   }
---
+++ src/components/Header.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+export default class Header extends React.Component{
+	constructor(){
+		super();
+		this.state={
+			navigation: 1
+		}
+	}
+	handleClick(index){
+		let self= this;
+		console.log("index ",index,this.state);
+		this.setState({navigation: index},function(){
+			self.props.callback(index);
+		}.bind(self));
+	}
+
+	renderLi(name,index,context){
+		if(context.state.navigation==index){
+			return (
+				<li role="presentation" className="active" onClick={context.handleClick.bind(context,index)}><a href="#">{name}</a></li>
+			);
+		}
+		return (
+			<li role="presentation" onClick={context.handleClick.bind(context,index)}><a href="#">{name}</a></li>
+		);
+	}
+
+	render(){
+		let self= this;
+		return (
+			<div className="page-header">
+				<h1>Water Management System <small>Rainfall and water level prediction</small></h1>
+				<br/>
+				<ul className="nav nav-pills">
+					{self.renderLi("Home",1,self)}
+					{self.renderLi("View Map",2,self)}
+					{self.renderLi("Weather data",3,self)}
+					{self.renderLi("Contact us",4,self)}
+				</ul>
+			</div>
+		);
+	}
+}
